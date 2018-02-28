ed98b65c44ac4e8ed27243aa8d9b4076be44ef6f
mohafizz <mohafizz@gmail.com>
2018-02-28 14:22:00 +0800

    adding fizzbuzz test solution

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,9 +1,17 @@
 const fizzbuzz = require("../src/fizzbuzz");
 
-test("1 should return number 1", function() {
-  expect(fizzbuzz(1)).toEqual(1);
+test("Multiple of 3 should return 'fizz'", function() {
+  expect(fizzbuzz(3)).toEqual("fizz");
 });
 
-test.skip("3 should return fizz", function() {
-  expect(fizzbuzz(3)).toEqual("fizz");
+test("Mulitiple of 5 should return 'buzz'", function() {
+  expect(fizzbuzz(5)).toEqual("buzz");
+});
+
+test("Multiple of 15 should return 'fizzbuzz'", function() {
+  expect(fizzbuzz(15)).toEqual("fizzbuzz");
+});
+
+test("4 should return undefined", function() {
+  expect(fizzbuzz(4)).toEqual(undefined);
 });
